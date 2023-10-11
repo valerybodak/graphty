539c3fc57b94fc8580d936b39a0bf4dba479cf48
valerybodak <valery.bodak@gmail.com>
2023-10-11 18:05:32 +0300

    removed GraphtyUtils from MainActivity

--- app/src/main/java/com/vbodak/graphty/MainActivity.kt
+++ app/src/main/java/com/vbodak/graphty/MainActivity.kt
@@ -2,13 +2,10 @@ package com.vbodak.graphty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.vbodak.graphtylib.GraphtyUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        GraphtyUtils().drawGraph()
     }
 }
