9e721b83fc9e25afe31d5c659a019c9771e24226
valerybodak <valery.bodak@gmail.com>
2023-10-16 10:42:06 +0300

    updated settings

--- app/src/main/java/com/vbodak/graphty/MainActivity.kt
+++ app/src/main/java/com/vbodak/graphty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.vbodak.graphty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vbodak.graphty.databinding.ActivityMainBinding
+import com.vbodak.graphtylib.graph.week.NodesMode
 import com.vbodak.graphtylib.graph.week.Params
 import java.util.Calendar
 
@@ -15,15 +16,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupGraph()
-        displayGraph()
+        setupGraph1()
+        displayGraph1()
+
+        setupGraph2()
+        displayGraph2()
     }
 
-    private fun setupGraph() {
-        binding.weekLineGraph.setup(
+    private fun setupGraph1() {
+        binding.weekLineGraph1.setup(
             params = Params(
                 minValue = 10,
                 maxValue = 100,
+                lineColor = android.R.color.holo_green_light,
+                lineWidth= 4F,
+                nodeRadiusPx = 20F,
+                enableGuidelines = false,
+                nodesMode = NodesMode.NONE,
+                valueScaleWidthPx = 82F,
                 weekdayStart = Calendar.SUNDAY,
                 weekdayNameMap = mapOf(
                     Calendar.MONDAY to "M",
@@ -38,13 +48,48 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun displayGraph(){
-        binding.weekLineGraph.draw(
-            values = getValues()
+    private fun displayGraph1(){
+        binding.weekLineGraph1.draw(
+            values = getValues1()
         )
     }
 
-    private fun getValues(): List<Int>{
-        return listOf(10, 55, 25, 40, 45, 100, 78)
+    private fun getValues1(): List<Int>{
+        return listOf(17, 55, 15, 40, 16, 19, 97)
+    }
+
+    private fun setupGraph2() {
+        binding.weekLineGraph2.setup(
+            params = Params(
+                minValue = 10,
+                maxValue = 100,
+                lineColor = android.R.color.black,
+                lineWidth= 4F,
+                nodeRadiusPx = 20F,
+                nodeFillColor = android.R.color.background_light,
+                nodesMode = NodesMode.MAX,
+                valueScaleWidthPx = 82F,
+                weekdayStart = Calendar.SUNDAY,
+                weekdayNameMap = mapOf(
+                    Calendar.MONDAY to "M",
+                    Calendar.TUESDAY to "T",
+                    Calendar.WEDNESDAY to "W",
+                    Calendar.THURSDAY to "T",
+                    Calendar.FRIDAY to "F",
+                    Calendar.SATURDAY to "S",
+                    Calendar.SUNDAY to "S",
+                )
+            )
+        )
+    }
+
+    private fun displayGraph2(){
+        binding.weekLineGraph2.draw(
+            values = getValues2()
+        )
+    }
+
+    private fun getValues2(): List<Int>{
+        return listOf(7, 71, 69, 65, 93, 20, 27)
     }
 }
