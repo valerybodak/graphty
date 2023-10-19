cf3b7983c708c8d7745534ffbcc46003c7c327b5
valerybodak <valery.bodak@gmail.com>
2023-10-19 11:19:48 +0300

    removed @ColorRes

--- graphtylib/src/main/java/com/vbodak/graphtylib/graph/bar/BarGraphParams.kt
+++ graphtylib/src/main/java/com/vbodak/graphtylib/graph/bar/BarGraphParams.kt
@@ -4,7 +4,6 @@ import androidx.annotation.ColorRes
 import com.vbodak.graphtylib.graph.base.BaseGraphParams
 
 class BarGraphParams : BaseGraphParams() {
-    @ColorRes
     var barColors: List<Int> = listOf(
         android.R.color.holo_green_dark,
         android.R.color.holo_red_light,
