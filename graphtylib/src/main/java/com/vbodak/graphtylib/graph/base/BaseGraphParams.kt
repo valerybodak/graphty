0eaa98327a86a2519e617f6ce3d856003ea7e317
valerybodak <valery.bodak@gmail.com>
2023-10-19 10:50:52 +0300

    code refactoring

--- graphtylib/src/main/java/com/vbodak/graphtylib/graph/base/BaseGraphParams.kt
+++ graphtylib/src/main/java/com/vbodak/graphtylib/graph/base/BaseGraphParams.kt
@@ -8,6 +8,7 @@ open class BaseGraphParams {
      * Min value to display on scale
      */
     var minValue: Int = CommonConst.UNDEFINED
+
     /**
      * Max value to display on scale
      */
@@ -15,10 +16,12 @@ open class BaseGraphParams {
 
     var valueScaleWidthPx: Float = 100F
     var valueTextSize: Float = 36F
+
     @ColorRes
     var valueTextColor: Int = android.R.color.black
     var titleScaleHeightPx: Float = 70F
     var titleTextSize: Float = 36F
+
     @ColorRes
     var titleTextColor: Int = android.R.color.black
 }
--- graphtylib/src/main/java/com/vbodak/graphtylib/graph/week/WeekLineGraphParams.kt
+++ graphtylib/src/main/java/com/vbodak/graphtylib/graph/week/WeekLineGraphParams.kt
@@ -8,13 +8,16 @@ import java.util.*
 class WeekLineGraphParams : BaseGraphParams() {
     var enableGuidelines: Boolean = true
     var lineWidth: Float = 5F
+
     @ColorRes
     var lineColor: Int = android.R.color.black
     var guidelineWidth: Float = 3F
+
     @ColorRes
     var guidelineColor: Int = android.R.color.darker_gray
     var nodesMode: NodesMode = NodesMode.ALL
     var nodeRadiusPx: Float = 14F
+
     @ColorRes
     var nodeFillColor: Int = android.R.color.white
     var weekdayStart: Int = Calendar.MONDAY
